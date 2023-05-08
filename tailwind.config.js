5b3aa91763b1d41e8b26da90472884154e23e548
arsalanmalik7 <arsalantech277@gmail.com>
2023-05-08 18:00:41 +0500

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      spacing: {
+        '128': '80rem'
+      }
+    },
+
+    plugins: [],
+  }
+}
